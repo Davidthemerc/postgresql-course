7a3268768717607f384f2889f33d7af68ef8f008
Davidthemerc <dmerc90@gmail.com>
2022-10-20 21:48:43 -0700

    Week 12, Exercise 4

---
+++ week_12/exercise_4/exercise_4.sql
@@ -0,0 +1,3 @@
+SELECT pfirstname,plastname FROM people LIMIT 10;
+SELECT pfirstname,plastname,lname FROM people NATURAL LEFT JOIN lists LIMIT 10;
+SELECT pfirstname,plastname,lname,iname FROM people NATURAL LEFT JOIN lists NATURAL LEFT JOIN listitems NATURAL LEFT JOIN items LIMIT 10;
