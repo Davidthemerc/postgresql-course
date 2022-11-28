fa9c7c10dbaba07f879879eeec6229017a400f30
Davidthemerc <dmerc90@gmail.com>
2022-11-27 20:00:12 -0800

    Test

---
+++ week_16/exercise_4/exercise_4.sql
@@ -0,0 +1,2 @@
+SELECT pfirstname,plastname,ppoints FROM people WHERE ppoints > (SELECT avg(ppoints) FROM people) ORDER BY ppoints DESC;
+UPDATE people SET plastname = CONCAT(plastname,'+HERO') WHERE ppoints > (SELECT avg(ppoints) FROM people) RETURNING *;
