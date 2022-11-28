67dd7419ef83b89009b1f1b920933aed63f38ae9
Davidthemerc <dmerc90@gmail.com>
2022-11-27 20:02:25 -0800

    Week 16, Exercise 4, plus an all-commands rebuild due to a couple resets needed. This assignment alters a lot of data!

--- week_16/exercise_4/exercise_4.sql
+++ week_16/exercise_4/exercise_4.sql
@@ -1,2 +1,3 @@
 SELECT pfirstname,plastname,ppoints FROM people WHERE ppoints > (SELECT avg(ppoints) FROM people) ORDER BY ppoints DESC;
 UPDATE people SET plastname = CONCAT(plastname,'+HERO') WHERE ppoints > (SELECT avg(ppoints) FROM people) RETURNING *;
+/* This took a couple of all-commands resets!*/
